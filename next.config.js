843894604a1979e77c0e01fe14d76abf37e7c51b
Ruslan Gonzalez <ruslanguns@gmail.com>
2025-06-11 08:14:39 +0000

    ci: fix next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+import { withSentryConfig } from "@sentry/nextjs";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
@@ -7,13 +9,7 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {};
 
-export default config;
-
-// Injected content via Sentry wizard below
-
-import { withSentryConfig } from "@sentry/nextjs";
-
-module.exports = withSentryConfig(module.exports, {
+export default withSentryConfig(config, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
